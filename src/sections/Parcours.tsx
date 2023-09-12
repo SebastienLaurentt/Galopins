a6caeac6b9e3431f4dcc42224741856f9024b7f5
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-12 10:40:21 +0200

    add some content in parcours component and responsive

--- src/sections/Parcours.tsx
+++ src/sections/Parcours.tsx
@@ -7,7 +7,7 @@ import Header from '../components/Header';
 
 function Parcours() {
     const sectionStyle = {
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)), url(${ParcoursBg})`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${ParcoursBg})`,
     };
 
     return (
@@ -23,11 +23,44 @@ function Parcours() {
                     <h2>LES PARCOURS</h2>
                     <GiStonePath className="text-3xl md:text-4xl xl:text-5xl" />
                 </div>
-                <div className='text-center md:text-left md:px-8 xl:px-12 mb-4 md:mb-32'>
+                <div className='text-center md:text-left md:px-8 xl:px-12 mb-4 md:mb-16'>
                     <p>Les Galopins se déplacent toujours en covoiturage afin de limiter le nombre de voitures. Une participation est demandée à la personne transportée. Le covid nous obligeant d’être maximun à 4 par voiture le tarif est laissé à l’appréciation du conducteur (la base étant de 0.2€ du km).
                     </p>
-                    <p>Afin de garder un côté convivial, chaque mois un restaurant est organisé (le vendredi soir en principe). Début Janvier, nous nous réunissons pour la galette des rois  à la salle St Martin avec projection sur grand écran des photos prises lors de nos randonnées. En fin de saison nous nous réunissons en plein air pour l’assemblée générale suivie d’un repas. Nous puisons dans l’argent des cotisations afin de participer à ces évènements.
-                    </p>
+                    <p>Les randonnées ont lieu du 1er Septembre à la mi-Juin tous les lundis après midi et vendredis à la journée.</p>
+                </div>
+                <div className='flex flex-col xl:flex-row gap-y-8 md:justify-around text-center'>
+                    <div className='flex flex-col border rounded-lg p-4'>
+                        <h3>
+                            Lundi | Après - midi 
+                        </h3>
+                        <span className='italic mb-4'>
+                            Tous niveau
+                        </span>
+                        <ul >
+                            <li>
+                                - De 5 à 14 km
+                            </li>
+                            <li>
+                               - Dans un rayon de 40 km maximum entre la Drôme et l'Ardèche
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='flex flex-col border rounded-lg p-4'>
+                        <h3>
+                            Vendredi | Journée entière
+                        </h3>
+                        <span className='italic mb-4'>
+                            Niveau confirmé
+                        </span>
+                        <ul>
+                            <li>
+                                - De 15 à 20 km
+                            </li>
+                            <li>
+                               - Dans un rayon de 120 km maximum jusqu'au Vaucluse, Isère, Gard, Hérault ou Lozère
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </section>
