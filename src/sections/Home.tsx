d4b318cef9ae7c9dbe8eedac973262c74edb2429
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-12 10:12:21 +0200

    add some content in home

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -14,11 +14,11 @@ function Home() {
         >
             <Header/>
             {/* HERO CONTENT */}
-            <div className='p-8 md:p-24 xl:p-72  h-3/4 xl:h-2/3 flex flex-col justify-center items-center  mx-auto'>
+            <div className='p-8 md:p-24 xl:p-60   xl:h-3/4 flex flex-col justify-center items-center  mx-auto'>
                     <h1>LES GALOPINS</h1>
                     <p className='italic text-center mb-8'>Bienvenue sur notre site internet ! </p>
-                    <p className=' text-center'>Nous sommes un club de randonnée situé à Montélimar ayant pour but  la découverte de la nature et du patrimoine dans la bonne humeur et en toute convivialité. </p>
-                    <p className=' text-center'>Si cela vous intéresse, sentez vous libre d'explorer ce site afin d'en apprendre davantage sur notre <strong>club</strong>, les <strong>parcours que nous proposons</strong> mais aussi ce qui est <strong>nécessaire pour nous rejoindre</strong> ! </p>
+                    <p className=' text-center'>Nous sommes un club de randonnée situé à Montélimar, dans la Drôme, ayant pour but la découverte de la <strong>nature</strong> et du <strong>patrimoine</strong> dans la <strong>bonne humeur</strong> et en toute <strong>convivialité</strong>. Les randonnées ont lieu du <strong>1er Septembre au 15 Juin</strong>, chaque <strong>Lundi et Vendredi</strong> et sont organisées selon <strong>deux niveaux de difficultés</strong>.  </p>
+                    <p className=' text-center'>Si vous souhaitez en savoir davantage, sentez vous libre d'explorer ce site ! Vous y trouverez des informations plus précises concernant notre <strong>club</strong>, les <strong>parcours que nous proposons</strong> mais aussi ce qui est <strong>nécessaire pour nous rejoindre</strong> ! </p>
                     
                     <button className='border-2 p-2 rounded-lg md:hover:bg-white md:hover:text-black mt-8'>DOCUMENT D'ADMISSION</button>
             </div>
