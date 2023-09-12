766230b6b2e78796bbf5d7a8b47100b9883fb770
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-12 09:44:41 +0200

    add some content in club component and do responsive

--- src/sections/Club.tsx
+++ src/sections/Club.tsx
@@ -23,14 +23,17 @@ function Club() {
             <Header/>
             {/* Club CONTENT */}
             <div className='flex flex-col md:p-4 xl:p-8'>
-                <div className='flex items-center gap-x-4 mb-4 justify-center md:justify-start'>
+                <div className='flex items-center gap-x-4 mb-4 md:mb-16 justify-center md:justify-start'>
                     <h2>LE CLUB</h2>
                     <AiFillHome className="text-3xl md:text-4xl xl:text-5xl" />
                 </div>
-                <div className='text-center md:text-left md:px-8 xl:px-12 mb-4'>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit dicta maiores quae repellendus molestias laborum nobis asperiores quaerat, recusandae, sed placeat veniam. Qui aliquam delectus beatae? Cupiditate ea, ullam incidunt error culpa est optio ad odit officia, et voluptate quaerat placeat dolorem corrupti deleniti numquam aspernatur quod enim at ipsum.</p>
+                <div className='text-center md:text-left md:px-8 xl:px-12 mb-4 md:mb-32'>
+                    <p>Une fois par an, une sortie en bus est organisée et est ouverte aux non adhérents.  St Guilhèm le Désert, La St Baume, Les sentiers de Pagnol, les îles Frioul, Les Baux, les carrières d’ocre de Rustrel, Le pont du Gard, De Gordes à Fontaine de Vaucluse, L’étang du Vaccarès et les Stes Marie de la mer, la cité templière de La Couvertoirade. 
+                    </p>
+                    <p>Afin de garder un côté convivial, chaque mois un restaurant est organisé (le vendredi soir en principe). Début Janvier, nous nous réunissons pour la galette des rois  à la salle St Martin avec projection sur grand écran des photos prises lors de nos randonnées. En fin de saison nous nous réunissons en plein air pour l’assemblée générale suivie d’un repas. Nous puisons dans l’argent des cotisations afin de participer à ces évènements.
+                    </p>
                 </div>
-                <div className='flex flex-col md:flex-row gap-y-16 md:gap-y-0 md:justify-around '>
+                <div className='flex flex-col md:flex-row  md:gap-y-0 md:justify-around '>
                     <div className='flex flex-col items-center gap-y-2'>
                         <BsFillPenFill className="text-3xl md:text-4xl xl:text-5xl" />
                         <span>Crée en Septembre 2004</span>
