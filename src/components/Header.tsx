33f8cae5c55a2cfb76ee69bb945b9d2185441325
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-12 14:37:42 +0200

    replace dynamic map for header by a static one because of hard typescript issue (want to try deploy on vercel

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,56 @@
 import { Link } from 'react-scroll';
-import data from '../data/navData'
-
 
 
 
 function Header() {
     return (
       <header className="py-4 md:p-8 text-sm md:text-lg xl:text-xl">
-        <ul className="flex justify-center gap-x-4 md:gap-x-10 xl:gap-x-28">
-            {data.map((value) => {
-                return (
+        <ul className="flex justify-center gap-x- md:gap-x-10 xl:gap-x-28">
                     <li>
                         <Link
-                            to={value.to}
+                            to='Accueil'
                             smooth={true} 
                             className="cursor-pointer md:hover:underline"
                             >
-                            {value.content}
+                            Accueil
                         </Link>
                     </li>
-                );
-            })}
+                    <li>
+                        <Link
+                            to='Club'
+                            smooth={true} 
+                            className="cursor-pointer md:hover:underline"
+                            >
+                            Club
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to='Parcours'
+                            smooth={true} 
+                            className="cursor-pointer md:hover:underline"
+                            >
+                            Parcours
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to='Conseils'
+                            smooth={true} 
+                            className="cursor-pointer md:hover:underline"
+                            >
+                            Conseils
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to='Rejoindre'
+                            smooth={true} 
+                            className="cursor-pointer md:hover:underline"
+                            >
+                            Nous rejoindre
+                        </Link>
+                    </li>
         </ul>
     </header>
     );
