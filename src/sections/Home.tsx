60ae37100fa0ca0cd72e4b28ac8869c244b53e6e
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-13 02:11:50 +0200

    add admission pdf download in home

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,6 +1,8 @@
 import HomeBg from '../static/img/home.jpg';
 import Header from '../components/Header';
 
+import AdmissionFile from '../assets/Admission_Galopins.pdf'
+
 function Home() {
     const sectionStyle = {
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)), url(${HomeBg})`,
@@ -20,7 +22,13 @@ function Home() {
                     <p className=' text-center'>Nous sommes un club de randonnée situé à Montélimar, dans la Drôme, ayant pour but la découverte de la <strong>nature</strong> et du <strong>patrimoine</strong> dans la <strong>bonne humeur</strong> et en toute <strong>convivialité</strong>. Les randonnées ont lieu du <strong>1er Septembre au 15 Juin</strong>, chaque <strong>Lundi et Vendredi</strong> et sont organisées selon <strong>deux niveaux de difficultés</strong>.  </p>
                     <p className=' text-center'>Si vous souhaitez en savoir davantage, sentez vous libre d'explorer ce site ! Vous y trouverez des informations plus précises concernant notre <strong>club</strong>, les <strong>parcours que nous proposons</strong> mais aussi ce qui est <strong>nécessaire pour nous rejoindre</strong> ! </p>
                     
-                    <button className='border-2 p-2 rounded-lg md:hover:bg-white md:hover:text-black mt-8'>DOCUMENT D'ADMISSION</button>
+                    <a 
+                        className='border-2 p-2 rounded-lg md:hover:bg-white md:hover:text-black mt-8'
+                        href={AdmissionFile}
+                        download ="Admission_Galopins.pdf"
+                    >
+                        DOCUMENT D'ADMISSION
+                    </a>
             </div>
 
         </section>
