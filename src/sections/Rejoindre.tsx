c649d3c624bdc64794e03258018e1d51db8d6d35
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-14 08:19:21 +0200

    correct orthographe in rejoindre section

--- src/sections/Rejoindre.tsx
+++ src/sections/Rejoindre.tsx
@@ -25,7 +25,7 @@ function Rejoindre() {
                     <p className='mb-8'>Pour rejoindre le club il est nécessaire de fournir un certificat médical validant l'aptitude à la randonnée ainsi qu'une copie d'attestation de responsabilité civile ou carte FFRP.
                     </p>
                     <p className='mb-8'>Les chaussures de randonnées sont obligatoires (baskets non autorisées). Une polaire et un coupe vent sont conseillés selon la saison. Les accompagnateurs se réservent le droit de refuser les personnes mal équipées.</p>
-                    <p className='mb-8'>Un montant de 15€ ainsi que le formulaire d'inscription ci-dessous seront demandés afin de valiser l'adhésion. </p>
+                    <p className='mb-8'>Un montant de 15€ ainsi que le formulaire d'inscription ci-dessous seront demandés afin de valider l'adhésion. </p>
                     <DownloadLink 
                         file={AdmissionFile}
                         fileName="Admission_Galopins.pdf"
