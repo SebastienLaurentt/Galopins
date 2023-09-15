da5e5ef727cd648ad6ec3819cbaf2609c18a5937
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-15 02:45:08 +0200

    add ml on md and xl on section header

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -5,7 +5,7 @@ interface SectionHeaderProps {
 
 function SectionHeader ({sectionTitle, children}: SectionHeaderProps) {
     return (
-        <div className='flex items-center gap-x-2 md:gap-x-4 mt-4 mb-8  justify-center md:justify-start'>
+        <div className='flex items-center gap-x-2 md:gap-x-4 mt-12 mb-8 md:ml-4 xl:ml-8  justify-center md:justify-start'>
             <h2>{sectionTitle}</h2>
             {children}
         </div>
