43b055edf7c630b3ba7e31a9338fc270987b2cf4
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-15 02:55:32 +0200

    clean some details around all

--- src/sections/Club.tsx
+++ src/sections/Club.tsx
@@ -21,31 +21,30 @@ function Club() {
             <SectionHeader sectionTitle='LE CLUB'>
                 <AiFillHome className="icon"/>
             </SectionHeader>
-            {/* <div className='flex flex-col md:p-4 xl:p-8'> */}
             <SubSection>
-            <div className='text-center md:text-left md:px-8 xl:px-12 mb-4 md:mb-24'>
-                    <p>Une fois par an, une sortie en bus est organisée et est ouverte aux non adhérents.  St Guilhèm le Désert, La St Baume, Les sentiers de Pagnol, les îles Frioul, Les Baux, les carrières d’ocre de Rustrel, Le pont du Gard, De Gordes à Fontaine de Vaucluse, L’étang du Vaccarès et les Stes Marie de la mer, la cité templière de La Couvertoirade. 
-                    </p>
-                    <p>Afin de garder un côté convivial, chaque mois un restaurant est organisé (le vendredi soir en principe). Début Janvier, nous nous réunissons pour la galette des rois  à la salle St Martin avec projection sur grand écran des photos prises lors de nos randonnées. En fin de saison nous nous réunissons en plein air pour l’assemblée générale suivie d’un repas. Nous puisons dans l’argent des cotisations afin de participer à ces évènements.
-                    </p>
-                </div>
-                <ul className='md:flex md:flex-row md:justify-around '>
-                    <li>
-                        <ClubIcon iconDescription='Crée en Septembre 2004' >
-                            <BsFillPenFill className="icon" />
+                <div className='text-center md:text-left md:px-8 xl:px-12 mb-4 md:mb-24'>
+                        <p>Une fois par an, une sortie en bus est organisée et est ouverte aux non adhérents.  St Guilhèm le Désert, La St Baume, Les sentiers de Pagnol, les îles Frioul, Les Baux, les carrières d’ocre de Rustrel, Le pont du Gard, De Gordes à Fontaine de Vaucluse, L’étang du Vaccarès et les Stes Marie de la mer, la cité templière de La Couvertoirade. 
+                        </p>
+                        <p>Afin de garder un côté convivial, chaque mois un restaurant est organisé (le vendredi soir en principe). Début Janvier, nous nous réunissons pour la galette des rois  à la salle St Martin avec projection sur grand écran des photos prises lors de nos randonnées. En fin de saison nous nous réunissons en plein air pour l’assemblée générale suivie d’un repas. Nous puisons dans l’argent des cotisations afin de participer à ces évènements.
+                        </p>
+                    </div>
+                    <ul className='md:flex md:flex-row md:justify-around '>
+                        <li>
+                            <ClubIcon iconDescription='Crée en Septembre 2004' >
+                                <BsFillPenFill className="icon" />
+                            </ClubIcon>
+                        </li>
+                        <li>
+                            <ClubIcon iconDescription='70 adhérents en 2022' >
+                                <BsPeopleFill className="icon" />
+                            </ClubIcon>
+                        </li>
+                        <li>
+                        <ClubIcon iconDescription='200 randonnées en 2022' >
+                            <FaHiking className="icon" />
                         </ClubIcon>
-                    </li>
-                    <li>
-                        <ClubIcon iconDescription='70 adhérents en 2022' >
-                            <BsPeopleFill className="icon" />
-                        </ClubIcon>
-                    </li>
-                    <li>
-                    <ClubIcon iconDescription='200 randonnées en 2022' >
-                        <FaHiking className="icon" />
-                    </ClubIcon>
-                    </li>
-                </ul>
+                        </li>
+                    </ul>
             </SubSection>
 
         </Section>
--- src/sections/Parcours.tsx
+++ src/sections/Parcours.tsx
@@ -20,29 +20,29 @@ function Parcours() {
                 <GiStonePath className="icon"/>
             </SectionHeader>
             <SubSection>
-            <div className='text-center md:text-left md:px-8 xl:px-12 mb-4 md:mb-16'>
-                <p>Les Galopins se déplacent toujours en covoiturage afin de limiter le nombre de voitures. Une participation est demandée à la personne transportée. Le covid nous obligeant d’être maximun à 4 par voiture le tarif est laissé à l’appréciation du conducteur (la base étant de 0.2€ du km).
-                </p>
-                <p>Les randonnées ont lieu du 1er Septembre à la mi-Juin tous les lundis après midi et vendredis à la journée.</p>
-            </div>
-            <ul className='flex flex-col xl:flex-row gap-y-8 md:justify-around text-center'>
-                <li>
-                    <ParcoursCard 
-                        day='Lundi | Après - midi'
-                        level='Tous niveaux'
-                        description1='- De 5 à 14 km'
-                        description2="- Dans un rayon de 40 km maximum entre la Drôme et l'Ardèche"
-                    />
-                </li>
-                <li>
-                    <ParcoursCard 
-                        day='Vendredi | Journée entière'
-                        level='Niveau confirmé'
-                        description1='- De 15 à 20 km'
-                        description2="- Dans un rayon de 120 km maximum jusqu'au Vaucluse, Isère, Gard, Hérault ou Lozère"
-                    />
-                </li>
-            </ul>
+                <div className='text-center md:text-left md:px-8 xl:px-12 mb-4 md:mb-16'>
+                    <p>Les Galopins se déplacent toujours en covoiturage afin de limiter le nombre de voitures. Une participation est demandée à la personne transportée. Le covid nous obligeant d’être maximun à 4 par voiture le tarif est laissé à l’appréciation du conducteur (la base étant de 0.2€ du km).
+                    </p>
+                    <p>Les randonnées ont lieu du 1er Septembre à la mi-Juin tous les lundis après midi et vendredis à la journée.</p>
+                </div>
+                <ul className='flex flex-col xl:flex-row gap-y-8 md:justify-around text-center'>
+                    <li>
+                        <ParcoursCard 
+                            day='Lundi | Après - midi'
+                            level='Tous niveaux'
+                            description1='- De 5 à 14 km'
+                            description2="- Dans un rayon de 40 km maximum entre la Drôme et l'Ardèche"
+                        />
+                    </li>
+                    <li>
+                        <ParcoursCard 
+                            day='Vendredi | Journée entière'
+                            level='Niveau confirmé'
+                            description1='- De 15 à 20 km'
+                            description2="- Dans un rayon de 120 km maximum jusqu'au Vaucluse, Isère, Gard, Hérault ou Lozère"
+                        />
+                    </li>
+                </ul>
             </SubSection>
 
         </Section>
