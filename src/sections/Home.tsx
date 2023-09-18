512073877ffa2f549880eb515cd6e51fb0ea3d55
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-18 04:16:03 +0200

    add text center on SubSection Home component to tab and desk display

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -16,7 +16,7 @@ function Home() {
             <h1>LES GALOPINS</h1>
             {/* HERO CONTENT */}
             <SubSection
-                className='text-center items-center'
+                className='text-center md:text-center items-center'
             >
                 <p className='italic'>Bienvenue sur notre site internet ! </p>
                 <p>Nous sommes un club de randonnée situé à Montélimar, dans la Drôme, ayant pour but la découverte de la <strong>nature</strong> et du <strong>patrimoine</strong> dans la <strong>bonne humeur</strong> et en toute <strong>convivialité</strong>. Les randonnées ont lieu du <strong>1er Septembre au 30 Juin</strong>, les <strong>Lundis après-midi</strong> en alternance dans la Drôme et l'Ardèche ainsi que les   <strong>Vendredis</strong>, à destination de la Drôme, l'Ardèche, le Gard ou encore le Vaucluse.  </p>
