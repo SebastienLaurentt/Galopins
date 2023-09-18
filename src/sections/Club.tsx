6533d34d95529883cfb748e8c3b6d1600a4c724d
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-18 04:47:24 +0200

    update of some content

--- src/sections/Club.tsx
+++ src/sections/Club.tsx
@@ -21,7 +21,7 @@ function Club() {
             </SectionHeader>
             <SubSection>
                 <div className=''>
-                        <p>Notre club est une association à but non lucratif régie par la loi de 1901.  Des randonnées sont  proposées le Lundi après-midi et le Vendredi à la journée. Le Lundi, deux niveaux de difficultés sont possible : 
+                        <p>Notre club est une association à but non lucratif régie par la loi de 1901.  Des randonnées sont  proposées le Lundi après-midi et le Vendredi à la journée. Le Lundi, deux niveaux de difficultés sont possibles : 
                         </p>
                         {/* <p>St Guilhèm le Désert, La St Baume, Les sentiers de Pagnol, les îles Frioul, Les Baux, les carrières d’ocre de Rustrel, Le pont du Gard, De Gordes à Fontaine de Vaucluse, L’étang du Vaccarès et les Stes Marie de la mer, la cité templière de La Couvertoirade. 
                             
@@ -33,23 +33,23 @@ function Club() {
                         <ParcoursCard 
                             day='Lundi | Après - midi'
                             level='2 niveaux'
-                            description1='- Cool : 5 à 10 kms (2-3h environ)'
-                            description2="- Plus difficile : 10 à 13 kms (200 à 300m de dénivelé)"
+                            description1='- Cool : 5 à 9 kms (2-3h environ)'
+                            description2="- Plus difficile : 10 à 14 kms (200 à 500m de dénivelé)"
                         />
                     </li>
                     <li>
                         <ParcoursCard 
                             day='Vendredi | Journée entière'
                             level='Niveau confirmé'
-                            description1='- De 15 à 20 km'
-                            description2="- Dans un rayon de 120 km maximum jusqu'au Vaucluse, Isère, Gard, Hérault ou Lozère"
+                            description1="- Jusqu'à 15km ou de 15 à 20 km"
+                            description2="- Dans un rayon de 100 km maximum jusqu'au Vaucluse, Gard ou Lozère"
                         />
                     </li>
                 </ul>
                 <div className=''>
-                        <p>Une fois par an une sortie en bus est organisèe et est ouverte aux non-adhérents. Par exemple, des lieux tels que St Guilhèm le Désert, La St Baume, Les sentiers de Pagnol, mais aussi les carrières d’ocre de Rustrel, Le pont du Gard ou L’étang du Vaccarès ont dèja étés visités lors de ces rencontres. </p>
+                        <p>Une fois par an une sortie en bus est organisèe et est ouverte aux non-adhérents. Par exemple, des lieux tels que St Guilhèm le Désert, La St Baume, Les sentiers de Pagnol, mais aussi les carrières d’ocre de Rustrel, Le pont du Gard ou L’étang du Vaccarès ont dèja étés visités lors de ces sorties. </p>
                         <p>
-                            Afin de garder un côté convivial, tous les 2 mois un restaurant est organisé (le vendredi ou samedi soir). En début d'année, la galette des rois est aussi l'occasion de se retrouver pour partager un moment festif.  C'est également le cas à la fin Juin où un repas en plein air vient clôturer la fin de saison. Le club participe financierement à ces événements.
+                            Afin de garder un côté convivial, tous les 2 mois un restaurant est organisé (le vendredi soir ou samedi midi). En début d'année, la galette des rois est aussi l'occasion de se retrouver pour partager un moment festif.  C'est également le cas à la fin Juin où un repas en plein air vient clôturer la fin de saison. Le club participe financierement à ces événements.
                         </p>
                 </div>
             </SubSection>
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -19,7 +19,7 @@ function Home() {
                 className='text-center md:text-center items-center md:p-12 xl:p-20'
             >
                 <p className='italic'>Bienvenue sur notre site internet ! </p>
-                <p>Nous sommes un club de randonnée situé à Montélimar, dans la Drôme, ayant pour but la découverte de la <strong>nature</strong> et du <strong>patrimoine</strong> dans la <strong>bonne humeur</strong> et en toute <strong>convivialité</strong>. Les randonnées ont lieu du <strong>1er Septembre au 30 Juin</strong>, les <strong>Lundis après-midi</strong> en alternance dans la Drôme et l'Ardèche ainsi que les   <strong>Vendredis</strong>, à destination de la Drôme, l'Ardèche, le Gard ou encore le Vaucluse.  </p>
+                <p>Nous sommes un club de randonnée situé à Montélimar, dans la Drôme, ayant pour but la découverte de la <strong>nature</strong> et du <strong>patrimoine</strong> dans la <strong>bonne humeur</strong> et en toute <strong>convivialité</strong>. Les randonnées ont lieu du <strong>1er Septembre au 30 Juin</strong>, les <strong>Lundis après-midi</strong> en alternance dans la Drôme et l'Ardèche ainsi que les   <strong>Vendredis</strong>, à destination de la Drôme, l'Ardèche, le Gard, la Lozère ou encore le Vaucluse.  </p>
                 <p>Si vous souhaitez en savoir davantage, sentez vous libre d'explorer ce site ! Vous y trouverez des informations plus précises concernant notre <strong>club</strong>, les <strong>parcours que nous proposons</strong> mais aussi ce qui est <strong>nécessaire pour nous rejoindre</strong> ! </p>
                 <DownloadLink 
                     file={AdmissionFile}
