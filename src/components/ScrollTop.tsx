59ca26d58d4077efaa592b18fcec8c4204dfa352
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-19 14:23:10 +0200

    add some animation to ScrollTop button

--- src/components/ScrollTop.tsx
+++ src/components/ScrollTop.tsx
@@ -1,23 +1,35 @@
-
+import { useState } from 'react';
 import { Link } from 'react-scroll';
-
 import { FaArrowUp } from 'react-icons/fa';
 
 
-
 function ScrollTop() {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleHover = () => {
+        setIsActive(true);
+    }
+
+    const handleUnhover = () => {
+        setIsActive(false);
+    }
+
     return (
-        <div className='flex justify-center cursor-pointer md:hover:text-green-500 rounded-full '>
+        <div 
+            className={`flex justify-center cursor-pointer text-green-500 rounded-full ${isActive ? 'hovered' : ''}`}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleUnhover}
+        >
             <Link
                 to='Accueil'
                 smooth={true} 
                 className="flex flex-col items-center"
-                >
-                <FaArrowUp className='iconHeader'/>
+            >
+                <FaArrowUp className={`iconHeader ${isActive ? 'icon-hover' : ''}`} />
                 Accueil
             </Link>
         </div>
     );
-  }
-export default ScrollTop;
+}
 
+export default ScrollTop;
