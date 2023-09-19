af7fb0f39a3c88bbea6387bb8b890d1d5829830f
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-19 06:19:46 +0200

    parcoursCards bg change

--- src/components/ParcoursCard.tsx
+++ src/components/ParcoursCard.tsx
@@ -7,26 +7,27 @@ interface ParcoursCardProps {
 
 function ParcoursCard ({day, level, description1, description2}: ParcoursCardProps) {
     return (
-        <div className='flex flex-col rounded-2xl md:rounded-full xl:rounded-3xl p-4 md:p-8 bg-yellow-900 opacity-90'>
-            <h3>
-                {day}
-            </h3>
-            <span className='italic mb-4'>
-                {level}
-            </span>
-            <ul >
-                <li>
-                    <p className="mb-0">
-                        {description1}
-                    </p>
-
-                </li>
-                <li>
-                    <p className="mb-0">
-                        {description2}
-                    </p>
-                </li>
-            </ul>
+        <div className='flex flex-col'>
+            <div className='bg-green-600 bg-opacity-40 rounded-2xl md:rounded-full xl:rounded-3xl p-4 md:p-8'>
+                <h3 className="mb-0">
+                    {day}
+                </h3>
+                <span className='italic'>
+                    {level}
+                </span>
+                <ul className="mt-4">
+                    <li>
+                        <p className="mb-0">
+                            {description1}
+                        </p>
+                    </li>
+                    <li>
+                        <p className="mb-0">
+                            {description2}
+                        </p>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
