c3814ce28ebbe49b554aab0ca930f9d4fadccc38
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-21 09:41:06 +0200

    fix on img path to photos section

--- src/data/photosData.js
+++ src/data/photosData.js
@@ -1,19 +1,19 @@
 const data = [
       {
         id: 0,
-        mainImage: '../../src/static/img/club.webp',
+        mainImage: '/static/img/club.webp',
       },
       {
         id: 1,
-        mainImage: '../../src/static/img/home.webp',
+        mainImage: '/static/img/home.webp',
       },
       {
         id: 2,
-        mainImage: '../../src/static/img/parcours.webp',
+        mainImage: '/static/img/parcours.webp',
       },
       {
         id: 3,
-        mainImage: '../../src/static/img/rejoindre.webp',
+        mainImage: '/static/img/rejoindre.webp',
       },
     ];
   
