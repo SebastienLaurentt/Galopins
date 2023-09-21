3fb8fa5aae7e86b02cfe7da067963bc89d4cb4de
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-21 16:27:50 +0200

    in Header : move the icon from the li to the Link to improve ux click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,43 +11,43 @@ function Header() {
     return (
       <header className="">
         <ul className="flex justify-around md:justify-center md:gap-x-20 xl:gap-x-32 text-base md:text-lg xl:text-xl py-4 font-bold">
-            <li className='flex flex-col items-center cursor-pointer md:hover:text-green-500'>
-                <AiFillHome className='iconHeader'/>
+            <li className=''>
                 <Link
                     to='Club'
                     smooth={true} 
-                    className=""
+                    className="flex flex-col items-center cursor-pointer md:hover:text-green-500"
                     >
+                    <AiFillHome className='iconHeader'/>
                     Club
                 </Link>
             </li>
-            <li className='flex flex-col items-center cursor-pointer md:hover:text-green-500'>
-                <GiStonePath className='iconHeader' />
+            <li className=''>
                 <Link
                     to='Parcours'
                     smooth={true} 
-                    className=""
+                    className="flex flex-col items-center cursor-pointer md:hover:text-green-500"
                     >
+                    <GiStonePath className='iconHeader' />
                     Parcours
                 </Link>
             </li>
-            <li className='flex flex-col items-center cursor-pointer md:hover:text-green-500'>
-                <MdOutlinePhotoCamera className='iconHeader'/>
+            <li className=''>
                 <Link
                     to='Photos'
                     smooth={true} 
-                    className=""
+                    className="flex flex-col items-center cursor-pointer md:hover:text-green-500"
                     >
+                    <MdOutlinePhotoCamera className='iconHeader'/>
                     Photos
                 </Link>
             </li>
-            <li className='flex flex-col items-center cursor-pointer md:hover:text-green-500'>
-                <LuBackpack className='iconHeader' />
+            <li className=''>
                 <Link
                     to='Rejoindre'
                     smooth={true} 
-                    className=""
+                    className="flex flex-col items-center cursor-pointer md:hover:text-green-500"
                     >
+                    <LuBackpack className='iconHeader' />
                     Adhésion
                 </Link>
             </li>
