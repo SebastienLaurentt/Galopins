6dd52dea5b574b27072d9ffac02a22232a016a09
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-22 07:54:14 +0200

    improve hover effect on ScrollTop (now working with accueil hover)

--- src/components/ScrollTop.tsx
+++ src/components/ScrollTop.tsx
@@ -2,30 +2,17 @@ import { useState } from 'react';
 import { Link } from 'react-scroll';
 import { FaArrowUp } from 'react-icons/fa';
 
-
 function ScrollTop() {
-    const [isActive, setIsActive] = useState(false);
-
-    const handleHover = () => {
-        setIsActive(true);
-    }
-
-    const handleUnhover = () => {
-        setIsActive(false);
-    }
+    const [hovered, setHovered] = useState(false);
 
     return (
         <div 
-            className={`w-12 mx-auto flex justify-center cursor-pointer md:hover:text-green-500 rounded-full ${isActive ? 'hovered' : ''}`}
-            onMouseEnter={handleHover}
-            onMouseLeave={handleUnhover}
+            className="w-12 mx-auto flex justify-center cursor-pointer md:hover:text-green-500 rounded-full"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
         >
-            <Link
-                to='Accueil'
-                smooth={true} 
-                className="flex flex-col items-center font-bold"
-            >
-                <FaArrowUp className={`iconHeader ${isActive ? 'icon-hover' : ''}`} />
+            <Link to='Accueil' smooth={true} className="flex flex-col items-center font-bold">
+                <FaArrowUp className={`iconHeader transition-transform duration-300 ${hovered ? 'transform -translate-y-1' : ''}`} />
                 Accueil
             </Link>
         </div>
