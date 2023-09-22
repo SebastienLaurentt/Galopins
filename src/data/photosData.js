cf6dcf3a3660a228cf88968546c717ee976fa6d2
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-22 15:51:00 +0200

    add new photos on photosData

--- src/data/photosData.js
+++ src/data/photosData.js
@@ -1,19 +1,31 @@
 const data = [
+      // {
+      //   id: 0,
+      //   mainImage: '/static/img/club.webp',
+      // },
+      // {
+      //   id: 1,
+      //   mainImage: '/static/img/home.webp',
+      // },
+      // {
+      //   id: 2,
+      //   mainImage: '/static/img/parcours.webp',
+      // },
+      // {
+      //   id: 3,
+      //   mainImage: '/static/img/rejoindre.webp',
+      // },
       {
         id: 0,
-        mainImage: '/static/img/club.webp',
+        mainImage: '/static/img/groupe.webp',
       },
       {
         id: 1,
-        mainImage: '/static/img/home.webp',
+        mainImage: '/static/img/chevaux.webp',
       },
       {
         id: 2,
-        mainImage: '/static/img/parcours.webp',
-      },
-      {
-        id: 3,
-        mainImage: '/static/img/rejoindre.webp',
+        mainImage: '/static/img/montagne.webp',
       },
     ];
   
