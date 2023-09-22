e0f333ae449f0e3705b2451ce00acda864635b97
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-22 16:10:47 +0200

    fix typo in home

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -22,7 +22,7 @@ function Home() {
                 className='text-center md:text-center items-center md:p-12 xl:p-20'
             >
                 <h3 className='italic'>Bienvenue sur notre site internet ! </h3>
-                <p>Nous sommes un club de randonnée situé à Montélimar, dans la Drôme, ayant pour but la découverte de la <strong>nature</strong> et du <strong>patrimoine</strong> dans la <strong>bonne humeur</strong> et en toute <strong>convivialité</strong>. Les randonnées ont lieu du <strong>1er Septembre au 30 Juin</strong>, les <strong>Lundis après-midi</strong> en alternance dans la Drôme et l'Ardèche ainsi qu'un'   <strong>Vendredi</strong> sur deux, à destination de la Drôme, l'Ardèche, le Gard, la Lozère ou encore le Vaucluse.  </p>
+                <p>Nous sommes un club de randonnée situé à Montélimar, dans la Drôme, ayant pour but la découverte de la <strong>nature</strong> et du <strong>patrimoine</strong> dans la <strong>bonne humeur</strong> et en toute <strong>convivialité</strong>. Les randonnées ont lieu du <strong>1er Septembre au 30 Juin</strong>, les <strong>Lundis après-midi</strong> en alternance dans la Drôme et l'Ardèche ainsi qu'un   <strong>Vendredi</strong> sur deux, à destination de la Drôme, l'Ardèche, le Gard, la Lozère ou encore le Vaucluse.  </p>
                 <ul className='mt-4 flex md:flex-row md:justify-around '>
                         <li>
                             <ClubIcon iconDescription='Créé en Septembre 2004' >
