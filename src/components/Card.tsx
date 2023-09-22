61f673fb4019263b9e954c9d7776927ce60f2a08
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-22 10:12:33 +0200

    increase hover effect on card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ function Card({ mainImage, }: CardProps) {
 
     return (
             <div className="">
-                <img src={`${mainImage}`} alt="" className=" rounded-3xl transition-all duration-400 hover:md:scale-[1.02] h-[8rem] md:h-[24rem] xl:h-[31rem] mx-auto" />
+                <img src={`${mainImage}`} alt="" className=" rounded-3xl transition-all duration-400 hover:md:scale-[1.04] h-[8rem] md:h-[24rem] xl:h-[31rem] mx-auto" />
             </div>
     );
   }
