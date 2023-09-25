40bc1cd167aee8c7ef90d717deca4750ec9f61da
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-25 13:49:02 +0200

    add animation on scrollTop component

--- src/components/ScrollTop.tsx
+++ src/components/ScrollTop.tsx
@@ -1,13 +1,34 @@
 import { useState } from 'react';
 import { Link } from 'react-scroll';
 import { FaArrowUp } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 function ScrollTop() {
     const [hovered, setHovered] = useState(false);
 
+    const fadeInAnimationsVariants = {
+        initial: {
+            opacity:0,
+            y:100,
+        },
+        animate: (id: number) => ({
+            opacity:1,
+            y:0,
+            transition:{
+                delay:0.15 * id,
+            },
+        }),
+    };
+
     return (
-        <div 
+        <motion.div 
             className="w-12 mx-auto flex justify-center cursor-pointer md:hover:text-green-500 rounded-full"
+            variants={fadeInAnimationsVariants}
+            initial="initial"
+            whileInView='animate'
+            viewport={{
+                once:true,
+            }}   
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
         >
@@ -15,7 +36,7 @@ function ScrollTop() {
                 <FaArrowUp className={`iconHeader transition-transform duration-300 ${hovered ? 'transform -translate-y-1' : ''}`} />
                 <span>Accueil</span>
             </Link>
-        </div>
+        </motion.div>
     );
 }
 
