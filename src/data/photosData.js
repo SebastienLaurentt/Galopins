ede72eb3fe96658d2bfa93732ffeece6b1b00f17
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-27 17:22:18 +0200

    add features in photos to select a date to displaying corresponding img

--- src/data/photosData.js
+++ src/data/photosData.js
@@ -1,20 +1,5 @@
 const data = [
-      // {
-      //   id: 0,
-      //   mainImage: '/static/img/club.webp',
-      // },
-      // {
-      //   id: 1,
-      //   mainImage: '/static/img/home.webp',
-      // },
-      // {
-      //   id: 2,
-      //   mainImage: '/static/img/parcours.webp',
-      // },
-      // {
-      //   id: 3,
-      //   mainImage: '/static/img/rejoindre.webp',
-      // },
+
       {
         id: 0,
         mainImage: '/static/img/groupe.webp',
--- src/sections/Photos.tsx
+++ src/sections/Photos.tsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
 import PhotosBg from '../static/img/photos.webp';
 import { MdOutlinePhotoCamera } from 'react-icons/md';
-
-// @ts-ignore
 import data from '../data/photosData.js';
 import SectionHeader from '../components/SectionHeader';
 import Section from '../components/Section';
@@ -20,6 +18,7 @@ interface PhotoData {
 
 function Photos() {
   const [zoomedImage, setZoomedImage] = useState<string | null>(null);
+  const [selectedDate, setSelectedDate] = useState<string>('date1');
 
   const handleZoomClick = (image: string) => {
     setZoomedImage(image);
@@ -37,15 +36,62 @@ function Photos() {
     }
   }, [zoomedImage]);
 
+  const handleDateChange = (event) => {
+    setSelectedDate(event.target.value);
+  }
+
+  const dataDate1 = [
+    data[0],
+    data[1],
+    data[2],
+    // ... (ajoutez les autres images pour la date 1)
+  ];
+
+  const dataDate2 = [
+    data[3],
+    data[4],
+    data[5],
+    // ... (ajoutez les autres images pour la date 2)
+  ];
+
+  const dataToUse = selectedDate === 'date1' ? dataDate1 : dataDate2;
+
+  const locationInfo = [
+    {
+      date: 'date1',
+      location: 'Randonnée à Savasse'
+    },
+    {
+      date: 'date2',
+      location: 'Randonnée à Dieulefit'
+    },
+    // Ajoutez d'autres informations de lieu si nécessaire
+  ];
+
+  const getLocationInfo = (selectedDate) => {
+    const info = locationInfo.find(item => item.date === selectedDate);
+    return info ? info.location : '';
+  }
+
   return (
     <Section id='Photos' bg={PhotosBg}>
       <SectionHeader sectionTitle='NOS PHOTOS'>
         <MdOutlinePhotoCamera className="icon md:mt-4"/>
       </SectionHeader>
       <SubSection>
+        <div className="flex justify-center items-center mb-4">
+          <label htmlFor="dateSelect" className="mr-2 text-sm md:text-lg">Choisir une date :</label>
+          <select id="dateSelect" value={selectedDate} onChange={handleDateChange} className="p-2 border border-gray-300 rounded-md text-black text-sm md:text-lg">
+            <option value="date1">18/09/2023</option>
+            <option value="date2">25/09/2023</option>
+          </select>
+        </div>
+        <div className="text-center">
+          <p>{getLocationInfo(selectedDate)}</p>
+        </div>
         <div className="">
           <Carousel>
-            {data.map((value: PhotoData) => {
+            {dataToUse.map((value: PhotoData) => {
               return (
                 <SwiperSlide className="p-4" key={value.id}>
                   <Card 
