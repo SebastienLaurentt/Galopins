0764b246bb6688de738a71e8f23df49a676f8090
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-28 02:40:55 +0200

    select and upload photo is now dynamic from photosData.js

--- src/sections/Photos.tsx
+++ src/sections/Photos.tsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
 import PhotosBg from '../static/img/photos.webp';
 import { MdOutlinePhotoCamera } from 'react-icons/md';
-
-// @ts-ignore
 import data from '../data/photosData.js';
 import SectionHeader from '../components/SectionHeader';
 import Section from '../components/Section';
@@ -22,7 +20,7 @@ interface PhotoData {
 
 function Photos() {
   const [zoomedImage, setZoomedImage] = useState<string | null>(null);
-  const [selectedDate, setSelectedDate] = useState<string>('date1');
+  const [selectedDate, setSelectedDate] = useState<string>('rando1');
 
   const handleZoomClick = (image: string) => {
     setZoomedImage(image);
@@ -44,21 +42,7 @@ function Photos() {
     setSelectedDate(event.target.value);
   }
 
-  const dataDate1 = [
-    data[0],
-    data[1],
-    data[2],
-    // ... (ajoutez les autres images pour la date 1)
-  ];
-
-  const dataDate2 = [
-    data[3],
-    data[4],
-    data[5],
-    // ... (ajoutez les autres images pour la date 2)
-  ];
-
-  const dataToUse = selectedDate === 'date1' ? dataDate1 : dataDate2;
+  const dataToUse = data[selectedDate].photos;
 
   const handleDownloadClick = async () => {
     const imageUrls = dataToUse.map(item => item.mainImage);
@@ -91,23 +75,35 @@ function Photos() {
       <SubSection>
         <div className='flex flex-wrap justify-center items-center gap-x-6 gap-y-2'>
           <div className="flex flex-wrap gap-2 justify-center items-center">
-            <select id="dateSelect" value={selectedDate} onChange={handleDateChange} className=" md:cursor-pointer p-2 md:p-4 border border-gray-300 rounded-md text-black text-sm md:text-lg font-bold">
-              <option className='font-bold' value="date1">18/09/2023 - SAVASSE</option>
-              <option className='font-bold' value="date2">25/09/2023 - DIEULEFIT</option>
+            <select
+              id="dateSelect"
+              value={selectedDate}
+              onChange={handleDateChange}
+              className="md:cursor-pointer p-2 md:p-4 border border-gray-300 rounded-md text-black text-sm md:text-lg font-bold"
+            >
+              {Object.keys(data).map(key => (
+                <option
+                  key={key}
+                  className="font-bold"
+                  value={key}
+                >{`${data[key].date} - ${data[key].localisation}`}
+                </option>
+              ))}
             </select>
           </div>
           <div className="flex flex-row gap-2 justify-center items-center justify-center  md:cursor-pointer">
             <a 
-                  className={`flex p-2 rounded-lg md:text-lg 2xl:text-2xl md:p-4 md:hover:bg-green-600 bg-green-800`}
-                  onClick={handleDownloadClick}
-              >
-                  <div className='flex gap-x-2 items-center'>
-                      TÉLÉCHARGER
-                      <BiSolidDownload className=" text-lg md:text-2xl 2xl:text-3xl"/>
-                  </div>
+              className={`flex p-2 rounded-lg md:text-lg 2xl:text-2xl md:p-4 md:hover:bg-green-600 bg-green-800`}
+              onClick={handleDownloadClick}
+            >
+              <div className='flex gap-x-2 items-center'>
+                TÉLÉCHARGER
+                <BiSolidDownload className=" text-lg md:text-2xl 2xl:text-3xl"/>
+              </div>
             </a>
           </div>
         </div>
+
         <div className="">
           <Carousel>
             {dataToUse.map((value: PhotoData) => {
