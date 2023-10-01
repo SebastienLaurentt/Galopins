ddd8f9e4d8fa9501a097fc5b81dfb4e0bbce0e88
SebastienLaurentt <slaurent.26@gmail.com>
2023-10-01 21:41:55 +0200

    put pagination outside carrousel to allow centering of nav arrow

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -16,37 +16,42 @@ interface CustomSwiperStyles {
 function Carousel({ children }: CarouselProps) {
   const swiperStyles: CustomSwiperStyles = {
     "--swiper-navigation-color": "#FFFFFF",
+    "--swiper-navigation-size": "30px",
   };
   
   return (
-    <Swiper 
-      style={swiperStyles}
-      modules={[ Pagination, Navigation ]}
-      slidesPerView={1}
-      spaceBetween={16}
-      loop={false}
-      centeredSlides={false}
-      pagination={
-        {type:'fraction',}
-      }
-      navigation= {{
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }}
-      breakpoints={{
-        390: { slidesPerView: 1.0, spaceBetween: 8 },
-        768: { slidesPerView: 1.0, spaceBetween: 8 },
-        1024: { slidesPerView: 2.0, spaceBetween: 16 },
-        1280: { slidesPerView: 2.0, spaceBetween: 32 },
-      }}
-      className=""
-    >
-      <div className=''>
-        {children}
-      </div>
-      <div className="swiper-button-prev h-4"></div>
-      <div className="swiper-button-next h-4"></div>
-    </Swiper>
+    <div className=''>
+      <Swiper 
+        style={swiperStyles}
+        modules={[ Pagination, Navigation ]}
+        slidesPerView={1}
+        loop={false}
+        centeredSlides={false}
+        pagination={{
+          type:'fraction',
+          el: '.swiper-custom-pagination',
+        }}
+        navigation= {{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        breakpoints={{
+          390: { slidesPerView: 1.0, spaceBetween: 8 },
+          768: { slidesPerView: 1.0, spaceBetween: 8 },
+          1024: { slidesPerView: 2.0, spaceBetween: 8 },
+          1280: { slidesPerView: 2.0, spaceBetween: 32 },
+        }}
+        className="px-2 md:px-4"
+      >
+        <div className=''>
+          {children}
+        </div>
+        <div className="swiper-button-prev"></div>
+        <div className="swiper-button-next "></div>
+      </Swiper>
+      <div className="swiper-custom-pagination"/>
+      
+    </div>
   );
 }
 
