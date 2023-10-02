2c61ab1bb74904fd741c8d843b3674e1eeddb63c
SebastienLaurentt <slaurent.26@gmail.com>
2023-10-02 05:07:05 +0200

    use now justify-center for md display in ul of home, with gap-x in xl and 2xl display

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -35,7 +35,7 @@ function Home() {
             <h1>LES GALOPINS</h1>
             <h3 className='italic'>Bienvenue sur notre site internet ! </h3>
             <p className='px-4 md:px-20 wideScreen'>Nous sommes un club de randonnée situé à Montélimar, dans la Drôme, ayant pour but la découverte de la <strong>nature</strong> et du <strong>patrimoine</strong> dans la <strong>bonne humeur</strong> et en toute <strong>convivialité</strong>. Les randonnées ont lieu du <strong>1er Septembre au 30 Juin</strong>, les <strong>Lundis après-midi</strong> en alternance dans la Drôme et l'Ardèche ainsi que les <strong>Vendredis</strong>, à destination de la Drôme, l'Ardèche, le Gard, la Lozère ou encore le Vaucluse.  </p>
-            <ul className='flex md:flex-row md:justify-around wideScreen'>
+            <ul className='flex flex-row justify-around md:justify-center xl:gap-x-8 2xl:gap-x-24 wideScreen'>
                     <li>
                         <ClubIcon iconDescription='Créé en Septembre 2004' >
                             <BsFillPenFill className="icon" />
