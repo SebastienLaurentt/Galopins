1a9a2c9b527d8c59bb0b92963b119077848db2ec
SebastienLaurentt <slaurent.26@gmail.com>
2023-10-02 05:02:50 +0200

    typescript issue fix

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -6,24 +6,23 @@ import { BsFillPenFill } from 'react-icons/bs';
 import { BsPeopleFill } from 'react-icons/bs';
 import { FaHiking } from 'react-icons/fa';
 import Section from '../components/Section';
-import SubSection from '../components/SubSection';
 import ClubIcon from '../components/ClubIcon';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 
 function Home() {
-    const fadeInAnimationsVariants = {
-        initial: {
-            opacity:0,
-            y:100,
-        },
-        animate: () => ({
-            opacity:1,
-            y:0,
-            transition:{
-                delay:0.5,
-            },
-        }),
-    };
+    // const fadeInAnimationsVariants = {
+    //     initial: {
+    //         opacity:0,
+    //         y:100,
+    //     },
+    //     animate: () => ({
+    //         opacity:1,
+    //         y:0,
+    //         transition:{
+    //             delay:0.5,
+    //         },
+    //     }),
+    // };
 
     return (
         <Section
