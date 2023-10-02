95023e59124119eeb600d7af9e215e1d2aea8978
SebastienLaurentt <slaurent.26@gmail.com>
2023-10-02 13:49:00 +0200

    reduce mb on some div in parcours sections

--- src/sections/Parcours.tsx
+++ src/sections/Parcours.tsx
@@ -35,7 +35,7 @@ function Parcours() {
                     classname='md:ml-4'
                 />
             </div>
-            <div className='mb-4 px-4 md:px-12 xl:px-20 wideScreen'>
+            <div className='mb-2 px-4 md:px-12 xl:px-20 wideScreen'>
                 <p className='mb-3 2xl:mb-5'> Le détail des randonnées est : </p>
                 <ul className='list-disc text-start leading-7 xl:leading-9 md:ml-8'>
                     <li>
@@ -56,7 +56,7 @@ function Parcours() {
                     </li>
                 </ul>
             </div>
-            <div className='mb-8  flex flex-col items-center md:items-start px-4 md:px-12 xl:px-20 wideScreen'>
+            <div className='mb-4  flex flex-col items-center md:items-start px-4 md:px-12 xl:px-20 wideScreen'>
                 <p className='mb-3'>Le lieu de <strong>rendez-vous</strong> avant chaque départ est situé sur le parking en face du  95 Route de Valence (Bar le Provence). L'heure de
                 départ est fixée à 13h30.</p>
                 <Link 
@@ -66,7 +66,7 @@ function Parcours() {
                 />
             </div>
             <div className='px-4 md:px-12 xl:px-20 wideScreen'> 
-                <p>Les Galopins se déplacent toujours en <strong>covoiturage</strong> afin de limiter le nombre de voitures. Une participation est demandée à la personne transportée. Le tarif est laissé à l’appréciation du conducteur (la base étant de 0.2€ du km).
+                <p>Les Galopins se déplacent toujours en <strong>covoiturage</strong> afin de limiter le nombre de voitures. Le tarif est laissé à l’appréciation du conducteur (la base étant de 0.2€ du km).
                 </p>
             </div>
             <ScrollTop/>
