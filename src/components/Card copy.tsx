b5794417737bd369ec3273871390b05ec60ce43d
SebastienLaurentt <slaurent.26@gmail.com>
2023-11-20 03:23:20 +0100

    rework of photoComponent with API - Now working with pictures

--- src/components/Card copy.tsx
+++ src/components/Card copy.tsx
@@ -1,16 +1,19 @@
 
+
 interface CardProps {
   mainImage: string;
   description?: string;
+  onZoomClick: (image: string) => void;
 }
 
-function Card({ mainImage}: CardProps) {
+function Card({ mainImage, onZoomClick }: CardProps) {
   return (
     <div className="p-4 ">
       <img 
         src={mainImage} 
         alt="" 
         className="rounded-3xl transition-all duration-400 cursor-pointer "
+        onClick={() => onZoomClick(mainImage)}
       />
     </div>
   );
--- src/sections/Photos.tsx
+++ src/sections/Photos.tsx
@@ -9,6 +9,9 @@ import Section from '../components/Section';
 import SubSection from '../components/SubSection';
 import ScrollTop from '../components/ScrollTop';
 import axios from 'axios';
+import Carousel from '../components/Carousel';
+import Card from '../components/Card.js';
+import { SwiperSlide } from 'swiper/react';
 
 
 
@@ -66,8 +69,24 @@ function Photos() {
       </div>
 
         <div className='flex flex-wrap justify-center'>
-        <p>{selectedRandoData ? selectedRandoData.hp : "Chargement..."}</p>
+        <p>{selectedRandoData ? selectedRandoData.description : "Chargement..."}</p>
         </div>
+
+          
+        <div className="mb-8 xl:mb-4 ">
+          <Carousel>
+            {selectedRandoData ? selectedRandoData.pictures.map((value) => {
+              return (
+                <SwiperSlide className="" key={value}>
+                  <Card 
+                    key={value}
+                    mainImage={value}
+                  />
+                </SwiperSlide>
+              );
+            }) : "Chargement"}
+          </Carousel>
+        </div> 
 
 
       </SubSection>
