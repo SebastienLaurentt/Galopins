1f2d2408d65cb8635e943cce0b164f5286f780cc
SebastienLaurentt <slaurent.26@gmail.com>
2023-11-28 16:03:29 +0100

    add cookies for auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 
 import { Routes, Route } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 import MainPage from './sections/MainPage';
 import Login from './sections/Login';
 import Account from './sections/Account';
 import PrivateRoute from './components/PrivateRoute';
 import { useAuth } from './components/Auth';
+import { useEffect } from 'react';
 
 const App = () => {
-  const { isLogged }  = useAuth();
+  const { isLogged, login  }  = useAuth();
   console.log (`Is Logged = ${isLogged}`)
+
+  useEffect(() => {
+    const storedAuth = Cookies.get('isLogged');
+    if (storedAuth === 'true') {
+      // Restaure l'authentification au chargement de l'application
+      login();
+    }
+  }, [login]);
 
   return (
     <Routes>
--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,17 +1,20 @@
 // Auth.js
 import { createContext, useContext, useState } from 'react';
+import Cookies from 'js-cookie';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLogged, setIsLogged] = useState(false);
+  const [isLogged, setIsLogged] = useState(Cookies.get('isLogged') === 'true');
 
   const login = () => {
     setIsLogged(true);
+    Cookies.set('isLogged', 'true', { expires: 1/24 });
   };
 
   const logout = () => {
     setIsLogged(false);
+    Cookies.remove('isLogged');
   };
 
   return (
