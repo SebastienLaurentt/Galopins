6775a348635ea920614cd5fce621e346e94bf821
SebastienLaurentt <slaurent.26@gmail.com>
2023-12-21 03:36:57 +0100

    change mentions link style in club

--- src/sections/Club.tsx
+++ src/sections/Club.tsx
@@ -35,7 +35,7 @@ function Club() {
       </SectionHeader>
       <p className='px-4 md:px-20 wideScreen'>
         Notre club est une association à but non lucratif régie par la loi de 1901 ( 
-      <a href="mentions" className='italic text-green-600 underline md:hover:font-bold' >
+      <a href="mentions" className='italic  underline md:hover:font-bold' >
         voir les status des Galopins
       </a>
 
